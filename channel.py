5482fe9f612750a6252a7fccb26b1ce6e98933a1
irinaloskutova <l.irinka@mail.ru>
2023-03-04 23:02:35 +0500

    4 курс. 3 урок. 2 задание (class)

--- channel.py
+++ channel.py
@@ -37,20 +37,27 @@ class Channel:
         return channel
 
     def get_title(self):
+        """Получение названия канала"""
         return self.get_info().get('items')[0].get('snippet').get('title')
 
     def get_video_count(self):
+        """Получение количества видеороликов канала"""
         return self.get_info().get('items')[0].get('statistics').get('viewCount')
 
     def get_description(self):
+        """Получение описания канала"""
         return self.get_info().get('items')[0].get('snippet').get('description')
 
     def get_subscriber_count(self):
-        return self.get_info().get('items')[0].get('snippet').get('subscriberCount')
+        """Получение количества подписчиков канала"""
+        return self.get_info().get('items')[0].get('statistics').get('subscriberCount')
 
     def get_viewCount(self):
+        """Получение количества просмотров канала"""
         return self.get_info().get('items')[0].get('statistics').get('viewCount')
+
     def get_url(self):
+        """Записывает html адрес  канала"""
         return f"https://www.youtube.com/channel/{self.get_info().get('items')[0].get('id')}"
 
     def to_json(self, title):
@@ -64,18 +71,31 @@ class Channel:
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
+    def __repr__(self):
+        """Получаем название канала в формате Youtube-канал: <название_канала>"""
+        return f"Youtube-канал: {self.get_title()}"
+
+    def __len__(self):
+        return len(self.get_subscriber_count())
+
+    def __add__(self, other):
+        return self.get_subscriber_count() + other.get_subscriber_count()
+
+    def __gt__(self, other):
+        return self.get_subscriber_count() > other.get_subscriber_count()
 
 vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
 # print(vdud.print_info())
 # print(vdud.get_info())
-print(vdud.get_title())
+# print(vdud.get_title())
 
 # # получаем значения атрибутов
-print(vdud.title)
+# print(vdud.title)
 # вДудь
-print(vdud.video_count)
+# print(vdud.video_count)
+# print(vdud.subscriber_count)
 # 163
-print(vdud.url)
+# print(vdud.url)
 # https://www.youtube.com/channel/UCMCgOm8GZkHp8zJ6l7_hIuA
 #
 # # менять не можем
@@ -84,9 +104,16 @@ print(vdud.url)
 # AttributeError: property 'channel_id' of 'Channel' object has no setter
 #
 # # можем получить объект для работы с API вне класса
-print(Channel.get_service())
+# print(Channel.get_service())
 # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-
 
 # создать файл 'vdud.json' в данными по каналу
 # vdud.to_json('vdud.json')
+
+ch1 = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+ch2 = Channel('UC1eFXmJNkjITxPFWTy6RsWg')
+# print(ch1)
+# print(ch2)
+print(ch1 < ch2)
+print(ch1 > ch2)
+print(ch1 + ch2)
